97d476382fbef23f21399268bc125262fbba5040
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-05 22:49:24 -0400

    Store JWT on Login

--- src/pages/Home/Login.jsx
+++ src/pages/Home/Login.jsx
@@ -2,120 +2,110 @@ import React, { useState } from "react";
 import { Modal, PasswordInput, TextInput } from "@mantine/core";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { userApi } from "../../api/Api";
 
 function Login({ openedSignIn, setOpenedSignIn, setOpenedSignUp }) {
-    const [formError, setError] = useState({
-        password: "",
-        email: "",
-    }); 
+	const [formError, setError] = useState({
+		password: "",
+		email: "",
+	});
 
+	const navigate = useNavigate();
+	const [credentials, setCredentials] = useState({});
+	const handleChange = (e) => {
+		setCredentials({ ...credentials, [e.target.name]: e.target.value });
+	};
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		const email = credentials.email;
+		const password = credentials.password;
+		const loginUser = {
+			email: email,
+			password: password,
+			action: "login",
+		};
+		try {
+			const { data } = await axios.post(`${userApi}/login`, loginUser);
+			localStorage.setItem("token", data.token);
+		} catch (error) {
+			setError({ ...formError, email: error.response.data.err.message });
+		}
+	};
 
-    const navigate = useNavigate();
-    const [credentials, setCredentials] = useState({});
-    const handleChange = (e) => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value });
-    };
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        const email = credentials.email;
-        const password = credentials.password;
-        const loginUser = {
-            email: email,
-            password: password,
-            action: "login",
-        };
-        try {
-            const { data } = await axios.post(
-                "http://localhost:8080/login",
-                loginUser
-            );
-            const userType = data.user.role;
-            console.log(data);
-            if (userType === "user") navigate("/user");
-            else if (userType === "admin") navigate("/admin");
-        } catch (error) {
-            console.log(error.response.data.err.message);
-            if (error.response.data.err.message.includes("correo")) setError({...formError, email: error.response.data.err.message})
-            else setError({...formError, password: error.response.data.err.message, email: ""})
+	return (
+		<Modal
+			styles={{
+				title: {
+					fontSize: "34px",
+					marginBottom: "1rem",
+					letterSpacing: "1.2px",
+					fontWeight: "700",
+				},
+			}}
+			centered
+			opened={openedSignIn}
+			onClose={() => setOpenedSignIn(false)}
+			transition="fade"
+			transitionDuration={400}
+			position="center"
+			title="Iniciar Sesión"
+		>
+			<form className="form">
+				<TextInput
+					name="email"
+					label="Correo Electrónico"
+					value={credentials.email}
+					onInput={() => setError({ ...formError, email: "" })}
+					onChange={handleChange}
+					error={formError.email}
+				/>
 
-            
-        }
-    };
+				<PasswordInput
+					name="password"
+					label="Contraseña"
+					value={credentials.password}
+					onInput={() => setError({ ...formError, password: "" })}
+					onChange={handleChange}
+					error={formError.password}
+				/>
 
-    return (
-        <Modal
-            styles={{
-                title: {
-                    fontSize: "34px",
-                    marginBottom: "1rem",
-                    letterSpacing: "1.2px",
-                    fontWeight: "700",
-                },
-            }}
-            centered
-            opened={openedSignIn}
-            onClose={() => setOpenedSignIn(false)}
-            transition="fade"
-            transitionDuration={400}
-            position="center"
-            title="Iniciar Sesión"
-        >
-            <form className="form">
-                <TextInput
-                    name="email"
-                    label="Correo Electrónico"
-                    value={credentials.email}
-                    onInput={() => setError({...formError, email: ""})}
-                    onChange={handleChange}
-                    error={formError.email}
-                />
+				<button className="form-button" onClick={handleLogin}>
+					Ingresar
+				</button>
 
-                <PasswordInput
-                    name="password"
-                    label="Contraseña"
-                    value={credentials.password}
-                    onInput={() => setError({...formError, password: ""})}
-                    onChange={handleChange}
-                    error={formError.password}
-                />
+				<div className="modal-tools">
+					<div className="checkbox">
+						<input
+							type="checkbox"
+							value="lsRememberMe"
+							id="rememberMe"
+						/>
+						<label htmlFor="rememberMe">Recuérdame</label>
+					</div>
 
-                <button className="form-button" onClick={handleLogin}>
-                    Ingresar
-                </button>
+					<p className="modal-forgot">¿Olvidaste tu contraseña?</p>
+				</div>
 
-                <div className="modal-tools">
-                    <div className="checkbox">
-                        <input
-                            type="checkbox"
-                            value="lsRememberMe"
-                            id="rememberMe"
-                        />
-                        <label htmlFor="rememberMe">Recuérdame</label>
-                    </div>
-
-                    <p className="modal-forgot">¿Olvidaste tu contraseña?</p>
-                </div>
-
-                <div className = "modal-footer">
-                    <p className="modal-p">¿No tienes cuenta? </p>
-                    <p
-                        className="modal-p"
-                        onClick={() => {
-                            setOpenedSignIn(false);
-                            setOpenedSignUp(true);
-                            setCredentials({
-                                email: "",
-                                password: "",
-                                confirmPassword: "",
-                            });
-                        }}
-                    >
-                        Registrate
-                    </p>
-                </div>
-            </form>
-        </Modal>
-    );
+				<div className="modal-footer">
+					<p className="modal-p">¿No tienes cuenta? </p>
+					<p
+						className="modal-p"
+						onClick={() => {
+							setOpenedSignIn(false);
+							setOpenedSignUp(true);
+							setCredentials({
+								email: "",
+								password: "",
+								confirmPassword: "",
+							});
+						}}
+					>
+						Registrate
+					</p>
+				</div>
+			</form>
+		</Modal>
+	);
 }
 
 export default Login;
