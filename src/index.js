cbe4e32e855442c68ab4c01d344fa6c702439aa6
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-05 23:16:38 -0400

    removed dummy products

--- src/index.js
+++ src/index.js
@@ -3,114 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Datos dummy para probar hasta tener base de datos
-const menus = [
-  {
-    id: 1,
-    name: "Tabla de 20 piezas",
-    price: 9000,
-    stock: 0,
-    category: "tablas",
-    picture: "https://images.unsplash.com/photo-1615361200141-f45040f367be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-  },
-  {
-    id: 2,
-    name: "Tabla de 30 piezas",
-    price: 11200,
-    stock: 0,
-    category: "tablas",
-    picture: "https://images.unsplash.com/photo-1615361200098-9e630ec29b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-  },
-  {
-    id: 3,
-    name: "Tabla de 40 piezas",
-    price:  13400,
-    stock: 1,
-    category: "tablas",
-    picture: "https://images.unsplash.com/photo-1579584425555-c3ce17fd4351?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
-  },
-  {
-    id: 4,
-    name: "Tabla de 50 piezas",
-    price: 15600,
-    stock: 1,
-    category: "tablas",
-    picture: "https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-  },
-  {
-    id: 5,
-    name: "Tabla de 60 piezas",
-    price: 17800,
-    stock: 1,
-    category: "tablas",
-    picture: "https://images.unsplash.com/photo-1553621042-f6e147245754?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
-  },
-  {
-    id: 6,
-    name: "Koroke",
-    price: 4500,
-    stock: 0,
-    category: "entradas",
-    picture: "https://image.shutterstock.com/image-photo/broccoli-cream-croquette-japanese-style-600w-260895305.jpg"
-  },
-  {
-    id: 7,
-    name: "Gyosas",
-    price: 4500,
-    stock: 1,
-    category: "entradas",
-    picture: "https://images.unsplash.com/photo-1588182728399-e8f2df121744?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-  },
-  {
-    id: 8,
-    name: "Camaron apanado",
-    price: 4900,
-    stock: 1,
-    category: "entradas",
-    picture: "https://images.unsplash.com/photo-1579887829114-282b4fa31072?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-  },
-  {
-    id: 9,
-    name: "Gohan de pollo teriyaki",
-    price: 6000,
-    stock: 1,
-    category: "gohan",
-    picture: "https://media.istockphoto.com/photos/traditional-breakfast-of-japan-natt-gohan-picture-id174270968"
-  },
-  {
-    id: 10,
-    name: "Takikomi gohan",
-    price: 7000,
-    stock: 1,
-    category: "gohan",
-    picture: "https://media.istockphoto.com/photos/japanese-food-takikomi-gohan-boiled-rice-picture-id1202391123"
-  },
-  {
-    id: 11,
-    name: "Ramen 1",
-    price: 12000,
-    stock: 0,
-    category: "ramen",
-    picture: "https://images.unsplash.com/photo-1617421753170-46511a8d73fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
-  },
-  {
-    id: 12,
-    name: "Ramen 2",
-    price: 12000,
-    stock: 1,
-    category: "ramen",
-    picture: "https://images.unsplash.com/photo-1623341214825-9f4f963727da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-  },
-  {
-    id: 13,
-    name: "Ramen 3",
-    price: 12000,
-    stock: 1,
-    category: "ramen",
-    picture: "https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
-  }
-]
-
 const users = [
   {
     id: 1,
@@ -137,21 +29,21 @@ const orders = [
     id: 1,
     orderedBy: 1,
     orderDate: "2022/06/14 10:00:00",
-    content: [1,2,3],
+    content: ["62c4f0989f423e3c21ba35ca", "62c4f0f69f423e3c21ba35cb"],
     deliveryAddress: "Avenida Francia, Valparaíso",
   },
   {
     id: 2,
     orderedBy: 2,
     orderDate: "2022/06/14 10:04:32",
-    content: [3, 5],
+    content: ["62c4f0989f423e3c21ba35ca"],
     deliveryAddress: "Pedro Montt, Valparaíso"
   },
   {
     id: 3,
     orderedBy: 3,
     orderDate: "2022/06/14 10:06:17",
-    content: [11],
+    content: ["62c4f0989f423e3c21ba35ca", "62c4f0f69f423e3c21ba35cb"],
     deliveryAddress: "Avenida Francia, Valparaíso"
   },
 ]
