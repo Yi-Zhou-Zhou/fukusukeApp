2e9645084fcdbe20160e49c74d0922910f2ed6df
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-05 23:07:36 -0400

    Fixed catalog with new context

--- src/pages/User/Catalog.js
+++ src/pages/User/Catalog.js
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
+import { ProductContext } from "../../context/product/ProductContext";
 import { Badge, Button, Card, Container, Grid, Group, Image, Title, Text } from "@mantine/core";
 import { LoremIpsum } from 'react-lorem-ipsum';
 
 import stringifyPrice from '../../functions/common/stringifyPrice';
 
-const Catalog = ({ menus }) => {
-    const [initialMenus, setInitialMenus] = useState(menus)
+const Catalog = () => {
+
+    const { products } = useContext(ProductContext);
 
     return(
         <Container size = 'lg'>
             <Grid>
             {
-                initialMenus.map(menu => 
+                products.map(product => 
                     <div
-                        key = { menu.id }
+                        key = { product._id }
                         style = {{ width: '21rem', margin: '1rem' }}
                     >
                         <Card 
@@ -23,32 +25,27 @@ const Catalog = ({ menus }) => {
                             style = {{ height: '100%' }}
                         >
                             <Card.Section>
-                                <Image src = { menu.picture } alt = "Fotografía de comida" height={160}/>
+                                <Image src = { product.picture } alt = "Fotografía de comida" height={160}/>
                             </Card.Section>
 
                             <div style = {{ display: 'flex', margin: '1rem 0 0 0', justifyContent: 'space-between' }}>
                                 <div>
-                                    <Title order = {2} style = {{ fontSize: '1.125rem' }}> { menu.name } </Title>
+                                    <Title order = {2} style = {{ fontSize: '1.125rem' }}> { product.name } </Title>
 
                                     <Text> 
-                                        <LoremIpsum
-                                            p = {1}
-                                            avgSentencesPerParagraph = {1}
-                                            avgWordsPerSentence = {3}
-                                            startWithLoremIpsum = { false }
-                                        />
+                                        { product.description }
                                     </Text>
                                 </div>
 
                                 <div style = {{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '.5rem' }}>
-                                    <Text weight = { 600 }> ${ stringifyPrice(menu.price) } </Text>
+                                    <Text weight = { 600 }> ${ stringifyPrice(product.price) } </Text>
                                     <div style = {{ display: 'flex', alignItems: 'center' }}>
                                         <Button 
                                             compact
                                             style = {{ borderRadius: '5px 0 0 5px'}}
                                         > Agregar </Button>
                                         <Badge style = {{ height: '100%', borderRadius: '0 5px 5px 0' }}radius = 'xs'>
-                                            { Math.floor( Math.random() * 20)}
+                                            { product.stock }   
                                         </Badge>
                                     </div>
                                 </div>
