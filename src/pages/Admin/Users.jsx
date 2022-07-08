aa91e83780c0a98175a5e477cd3ac99ca2814650
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-08 16:04:31 -0400

    basic react-table

--- src/pages/Admin/Users.jsx
+++ src/pages/Admin/Users.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState, useMemo } from 'react'
 import {useParams} from 'react-router-dom'
 import {UserContext} from '../../context/user/UserContext'
-import { Table } from '@mantine/core'
+import { useTable } from 'react-table'
 
 const mapRouteToFilterName = (route) => {
   switch (route) {
@@ -18,51 +18,96 @@ const mapRouteToFilterName = (route) => {
   }
 }
 
+const Table = ({ users }) => {
+  const columns = useMemo(() => (
+    [
+      {
+        Header: 'Correo',
+        accessor: 'email',
+      },
+      {
+        Header: 'Nombre',
+        accessor: 'name',
+      },
+      {
+        Header: 'Run',
+        accessor: 'run',
+      },
+      {
+        Header: 'Celular',
+        accessor: 'phone',
+      },
+      {
+        Header: 'Rol',
+        accessor: 'role',
+      }
+    ]
+  ), [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data: users })
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        }
+        )}
+      </tbody>
+      <tfoot>
+        {footerGroups.map(footerGroup => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map(column => (
+              <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  )
+}
+
 function Users() {
   const selectedCategory = useParams().selectedCategory;
   const filterName = mapRouteToFilterName(selectedCategory);
   const {users} = useContext(UserContext);
 
-  const [filteredUsers, setFilteredUsers] = useState([]);
-
-  useEffect(() => {
-    if (filterName)
-      setFilteredUsers(users.filter(user => user.role === filterName));
-    else
-      setFilteredUsers(users);
-  }, [users, filterName]);
-
-  const rows = filteredUsers.map(user => {
-    return (
-      <tr key={user._id}>
-        <td>{user.email}</td>
-        <td>{user.name}</td>
-        <td>{user.run}</td>
-        <td>{user.phone}</td>
-        <td>{user.role}</td>
-      </tr>
-    )
-  });
+  const filteredUsers = useMemo(() => {
+    if (filterName) {
+      return users.filter(user => user.role === filterName)
+    }
+    return users
+  }, [users, filterName])
 
   return (
     <>
       {filterName}
-      <Table>
-        <thead>
-          <tr>
-            <th>Correo</th>
-            <th>Nombre</th>
-            <th>Run</th>
-            <th>Celular</th>
-            <th>Rol</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </Table>
+      <Table users={filteredUsers} />
     </>
   )
 }
 
-export default Users
+export default Users;
