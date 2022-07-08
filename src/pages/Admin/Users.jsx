3e28470a89475c96bfdaa348f77ace9a648e46c7
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-08 15:03:09 -0400

    users table (basic)

--- src/pages/Admin/Users.jsx
+++ src/pages/Admin/Users.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
+import {UserContext} from '../../context/user/UserContext'
+import { Table } from '@mantine/core'
 
 const mapRouteToFilterName = (route) => {
   switch (route) {
@@ -19,9 +21,47 @@ const mapRouteToFilterName = (route) => {
 function Users() {
   const selectedCategory = useParams().selectedCategory;
   const filterName = mapRouteToFilterName(selectedCategory);
+  const {users} = useContext(UserContext);
+
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  useEffect(() => {
+    if (filterName)
+      setFilteredUsers(users.filter(user => user.role === filterName));
+    else
+      setFilteredUsers(users);
+  }, [users, filterName]);
+
+  const rows = filteredUsers.map(user => {
+    return (
+      <tr key={user._id}>
+        <td>{user.email}</td>
+        <td>{user.name}</td>
+        <td>{user.run}</td>
+        <td>{user.phone}</td>
+        <td>{user.role}</td>
+      </tr>
+    )
+  });
 
   return (
-    <div>{filterName}</div>
+    <>
+      {filterName}
+      <Table>
+        <thead>
+          <tr>
+            <th>Correo</th>
+            <th>Nombre</th>
+            <th>Run</th>
+            <th>Celular</th>
+            <th>Rol</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </Table>
+    </>
   )
 }
 
