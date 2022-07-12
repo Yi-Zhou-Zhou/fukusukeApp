1fdea94e43faf2a6a08a013c039eb2119d415574
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-07-11 20:59:50 -0400

    delete user in db

--- src/pages/Admin/DeleteUser.jsx
+++ src/pages/Admin/DeleteUser.jsx
@@ -1,14 +1,27 @@
 import React, { useContext, useState } from "react";
 import { Modal, Group, Button } from "@mantine/core";
 import { UserContext } from "../../context/user/UserContext";
+import { userApi } from "../../api/Api";
+import axios from "axios";
 
 function DeleteUser({ _id, open, setOpen }) {
 	const { users, deleteUser } = useContext(UserContext);
 	const user = users.find((user) => user._id === _id);
 
-	const handleDelete = () => {
-		deleteUser(_id);
-		setOpen(false);
+	const handleDelete = async () => {
+		try {
+			// delete user in database
+			const response = await axios.delete(`${userApi}/${_id}/delete`, {
+				headers: {
+					"x-auth-token": localStorage.getItem("token"),
+				},
+			});
+			// delete user in context
+			deleteUser(_id);
+			setOpen(false);
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	return (
