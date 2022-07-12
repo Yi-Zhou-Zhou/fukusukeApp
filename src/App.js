50571e7317443144bee3e887ea3b754a51234747
PedroYanezE <pedro.yanez.e@gmail.com>
2022-07-11 23:27:15 -0400

    Ruta de catálogo y orden dummy para testear

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ import {
 } from 'react-router-dom';
 
 // User view
+import Catalog from './pages/User/Catalog';
+import Greeting from './pages/Home/Greeting';
 import Home from './pages/Home/Home';
+import Orders from './pages/User/Orders'
 
 // Admin view
 import AdminHome from './pages/Admin/AdminHome'
 import AdminGreeting from './pages/Admin/AdminGreeting'
-import Orders from './pages/Admin/Orders';
+import AdminOrders from './pages/Admin/AdminOrders';
 import Stock from './pages/Admin/Stock';
 import Users from "./pages/Admin/Users";
 
-import Greeting from './pages/Home/Greeting';
-import Catalog from './pages/User/Catalog';
 
 // Context
 import { ProductProvider } from "./context/product/ProductContext";
@@ -27,6 +28,23 @@ import { ProductProvider } from "./context/product/ProductContext";
 const App = ({ orders, users }) => {
     const [openedCart, setOpenedCart] = useState(false)
     const [cart, setCart] = useState([])
+    const [userOrders, setUserOrders] = useState([
+        [
+            id: 1,
+            {
+                "_id": "62c4f0f69f423e3c21ba35cb",
+                "name": "Tabla de 2 personas ",
+                "price": 20000,
+                "stock": 14,
+                "category": "tablas",
+                "picture": "https://images.unsplash.com/photo-1615361200098-9e630ec29b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+                "description": "15 Camarón, 15 Salmón",
+                "createdAt": "2022-07-12T01:10:47.178Z",
+                "updatedAt": "2022-07-12T01:10:47.178Z",
+                "cartQuantity": 4
+            }
+        ],
+      ])
 
     return (
         <ProductProvider>
@@ -45,7 +63,8 @@ const App = ({ orders, users }) => {
 
                     <Route path="/" element={<Home openedCart = { openedCart } setOpenedCart = { setOpenedCart }/>} >
                         <Route index element = { <Greeting/> } />
-                        <Route path = "catalogo" element = { <Catalog cart = { cart } setCart = { setCart } openedCart = { openedCart } setOpenedCart = { setOpenedCart } /> } />
+                        <Route path = "catalogo" element = { <Catalog cart = { cart } setCart = { setCart } openedCart = { openedCart } setOpenedCart = { setOpenedCart } orders = {userOrders} setOrders = { setUserOrders } /> } />
+                        <Route path = "pedidos" element = { <Orders orders = { userOrders } /> } />
                     </Route>
 
                 </Routes>
