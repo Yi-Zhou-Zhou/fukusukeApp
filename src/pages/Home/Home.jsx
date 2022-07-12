7452c68d5e35cb8bbf3b9a6fdc18fae8502d68be
PedroYanezE <pedro.yanez.e@gmail.com>
2022-07-11 23:25:15 -0400

    Se modificó la navbar para las distintas vistas de cliente

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,60 +2,140 @@ import React, { useState } from "react";
 
 import { AppShell } from "@mantine/core";
 
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import imgLogo from "../../images/logo.png";
-import { AiOutlineLogin } from "react-icons/ai";
-import { AiOutlineShoppingCart } from "react-icons/ai"
+import pdf from "../../pdf/Carta.pdf";
+
+import { AiOutlineLogin, AiOutlineShoppingCart, AiOutlineClockCircle } from "react-icons/ai";
+
 import { BiFoodMenu } from "react-icons/bi";
 import { RiTeamLine } from "react-icons/ri";
 import { GoLocation } from "react-icons/go";
-import pdf from "../../pdf/Carta.pdf";
+
 import Login from "./Login";
 import Register from "./Register";
 
 const HomeHeader = ({ setOpenedSignIn, handleOpenCart }) => {
-    return(
-        <nav className="navbar-menu">
-            <div className="navbar-container">
-                <div className="logo-container">
-                    <img className="logo" src={imgLogo} alt="fukusuke" />
-                </div>
+    let loc = useLocation()
 
-                <div className="navbar-links">
-                    <a href={pdf} target="_blank" className="navbar-link">
-                        <BiFoodMenu />
-                        Carta
-                    </a>
-                    <a href="#" className="navbar-link">
-                        <RiTeamLine />
-                        Nosotros
-                    </a>
-                    <a href="#" className="navbar-link">
-                        <GoLocation />
-                        Contacto
-                    </a>
-                    <button
-                        href = "#" 
-                        className = "navbar-link"
-                        style = {{ border: '0', background: 'transparent', cursor: 'pointer' }} 
-                        onClick = { handleOpenCart }
-                    >
-                        <AiOutlineShoppingCart />
-                        Carrito
-                    </button>
-                    <i
-                        onClick={() => {
-                            setOpenedSignIn(true);
-                        }}
-                        className="navbar-icon"
-                    >
-                        <AiOutlineLogin size={35} color="white" />
-                    </i>
+    if(loc.pathname === "/")
+    {
+        return(
+            <nav className="navbar-menu">
+                <div className="navbar-container">
+                    <div className="logo-container">
+                        <Link to="/">
+                            <img className="logo" src={imgLogo} alt="fukusuke" />
+                        </Link>
+                    </div>
+    
+                    <div className="navbar-links">
+                        <a href={pdf} target="_blank" className="navbar-link">
+                            <BiFoodMenu />
+                            Carta
+                        </a>
+
+                        <a href="#" className="navbar-link">
+                            <RiTeamLine />
+                            Nosotros
+                        </a>
+
+                        <a href="#" className="navbar-link">
+                            <GoLocation />
+                            Contacto
+                        </a>
+
+                        <Link to = "/pedidos" className="navbar-link">
+                            <AiOutlineClockCircle />
+                            Pedidos
+                        </Link>
+
+                        <i
+                            onClick={() => {
+                                setOpenedSignIn(true);
+                            }}
+                            className="navbar-icon"
+                        >
+                            <AiOutlineLogin size={35} color="white" />
+                        </i>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    } else if(loc.pathname === "/catalogo")
+    {
+        return(
+            <nav className="navbar-menu">
+                <div className="navbar-container">
+                    <div className="logo-container">
+                        <Link to="/">
+                            <img className="logo" src={imgLogo} alt="fukusuke" />
+                        </Link>
+                    </div>
+    
+                    <div className="navbar-links">
+                        <button
+                            href = "#" 
+                            className = "navbar-link"
+                            style = {{ border: '0', background: 'transparent', cursor: 'pointer' }} 
+                            onClick = { handleOpenCart }
+                        >
+                            <AiOutlineShoppingCart />
+                            Carrito
+                        </button>
+
+                        <Link to = "/pedidos" className="navbar-link">
+                            <AiOutlineClockCircle />
+                            Pedidos
+                        </Link>
+
+                        <i
+                            onClick={() => {
+                                setOpenedSignIn(true);
+                            }}
+                            className="navbar-icon"
+                        >
+                            <AiOutlineLogin size={35} color="white" />
+                        </i>
+                    </div>
+                </div>
+            </nav>
+        )
+    } else {
+        return(
+            <nav className="navbar-menu">
+                <div className="navbar-container">
+                    <div className="logo-container">
+                        <Link to="/">
+                            <img className="logo" src={imgLogo} alt="fukusuke" />
+                        </Link>
+                    </div>
+    
+                    <div className="navbar-links">
+                        <Link to = "/catalogo" className="navbar-link">
+                            <BiFoodMenu />
+                            Catalogo
+                        </Link>
+
+                        <Link to = "/pedidos" className="navbar-link">
+                            <AiOutlineClockCircle />
+                            Pedidos
+                        </Link>
+
+                        <i
+                            onClick={() => {
+                                setOpenedSignIn(true);
+                            }}
+                            className="navbar-icon"
+                        >
+                            <AiOutlineLogin size={35} color="white" />
+                        </i>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
 }
 
 const Home = ({ openedCart, setOpenedCart }) => {
