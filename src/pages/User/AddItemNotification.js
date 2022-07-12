b5ac57cbeb099a8af326098f92594f3843da504f
PedroYanezE <pedro.yanez.e@gmail.com>
2022-07-11 23:23:58 -0400

    Notificación que le indica al usuario que agregó un producto al carrito correctamente

--- src/pages/User/AddItemNotification.js
+++ src/pages/User/AddItemNotification.js
@@ -2,13 +2,20 @@ import React from "react";
 
 import { Center, Notification, Transition } from "@mantine/core";
 
-const AddItemNotification = ({ opened }) => {
+const AddItemNotification = ({ notificationMessage, showAddNotification, setShowAddNotification }) => {
     return(
-        <Transition mounted = { opened } duration = {250} timingFunction = "ease" transition = "slide-up">
+        <Transition mounted = { showAddNotification } duration = {250} timingFunction = "ease" transition = "slide-up">
             {
                 (styles) => 
-                    <Center style = {{ ...styles, margin: '1rem 0 0 0' }}> 
-                        <Notification style = {{ width: '32rem' }} color = "red" title = "¡Producto agregado al carrito correctamente!" />
+                    <Center 
+                        style = {{ ...styles, margin: '1rem 0 0 0' }}
+                    > 
+                        <Notification
+                            style = {{ width: 'fit-content' }}
+                            color = "red"
+                            title = { notificationMessage }
+                            onClose = { () => setShowAddNotification(false) }
+                        />
                     </Center>
             }
         </Transition>
