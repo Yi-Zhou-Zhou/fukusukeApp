e6fbf35837961fc346fb976b750f959e6365d39c
PedroYanezE <pedro.yanez.e@gmail.com>
2022-07-13 23:55:12 -0400

    Arreglé el posicionamiento de AddItemNotification

--- src/pages/User/AddItemNotification.js
+++ src/pages/User/AddItemNotification.js
@@ -4,11 +4,25 @@ import { Center, Notification, Transition } from "@mantine/core";
 
 const AddItemNotification = ({ notificationMessage, showAddNotification, setShowAddNotification }) => {
     return(
-        <Transition mounted = { showAddNotification } duration = {250} timingFunction = "ease" transition = "slide-up">
+        <Transition 
+            mounted = { showAddNotification }
+            duration = {250}
+            timingFunction = "ease"
+            transition = "slide-up"
+        >
             {
                 (styles) => 
                     <Center 
-                        style = {{ ...styles, margin: '1rem 0 0 0' }}
+                        style = {{ 
+                            ...styles, 
+                            position: 'fixed',
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                            marginBottom: '2rem'
+                        }}
                     > 
                         <Notification
                             style = {{ width: 'fit-content' }}
